12d469d42a0078168f1c0aae5df9fbf060291952
Olgu Batın Topal <80317793+Olgu-Batin-Topal@users.noreply.github.com>
2025-06-19 13:16:47 +0300

    BGColor Update

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -3,7 +3,7 @@ import { LoadingOutlined } from "@ant-design/icons";
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-zinc-100">
       <LoadingOutlined />
     </div>
   );
