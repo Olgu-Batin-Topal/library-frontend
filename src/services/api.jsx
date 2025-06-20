bfe47f4cd9788d461bbbd0a6a85ab2159a60935d
Olgu Batın Topal <olgu@ekads.co>
2025-06-20 23:25:46 +0300

    Env

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://api.library.test/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",
   },
